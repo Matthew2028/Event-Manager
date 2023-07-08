4601c4f2030a9bb19b0e64d1f15c84f860ad658a
Matthew2028 <72547588+Matthew2028@users.noreply.github.com>
2023-07-08 15:52:36 +0200

    adding firebase

--- event-manager/src/environments/environment.prod.ts
+++ event-manager/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'event-manager-b65d6',
+    appId: '1:573370547983:web:c931d19566ad2bab74c87a',
+    storageBucket: 'event-manager-b65d6.appspot.com',
+    apiKey: 'AIzaSyCKmErrZOUbWqcEHaFJ6pvdJJuP4t4ythg',
+    authDomain: 'event-manager-b65d6.firebaseapp.com',
+    messagingSenderId: '573370547983',
+    measurementId: 'G-YK92WH9SWK',
+  },
   production: true
 };
